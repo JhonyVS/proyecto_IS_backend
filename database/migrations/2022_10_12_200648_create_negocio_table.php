4f0922c00d19f7365e3df1c69332e11e30261748
Rustazaga <nevermorewars@gmail.com>
2022-10-12 17:01:24 -0400

    migracion de la tabla negocio usando foreign keys

---
+++ database/migrations/2022_10_12_200648_create_negocio_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('negocio', function (Blueprint $table) {
+            $table->bigIncrements('id_Negocio');
+            $table->unsignedBigInteger('id_Usuario');
+            $table->string('nombre',16);
+            $table->string('descrip',100);
+            $table->string('ubicacion',50);
+            $table->unsignedBigInteger('telefono');
+            $table->boolean('activo');
+            //$table->timestamps();
+            $table->foreign('id_Usuario')->references('id_Usuario')->on('Usuario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('negocio');
+    }
+};
