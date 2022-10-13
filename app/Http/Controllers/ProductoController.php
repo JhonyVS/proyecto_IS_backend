a1c77d7e47f8b5d570b8630eadbf2599de5f27e7
Aldair Torrez <torrezaldair777@gmail.com>
2022-10-13 17:56:56 -0400

    correccion de nombres de atributos y datos estáticos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Producto;
-use App\Models\Descuento;
+use App\Models\Promocion;
+use App\Models\Negocio;
+use App\Models\Usuario;
 
 class ProductoController extends Controller
 {
@@ -36,61 +38,77 @@ class ProductoController extends Controller
      */
     public function store(Request $request)
     {
-        $messages = [
-            'required' => 'El campo :attribute es obligatorio',
-            'unique' => 'El nombre :input ya fue tomado',
-            // 'equipos.*.preinscripcion_numero_de_deposito.unique' => 'El numero de transaccion :input ya uso en otro equipo',
-            // 'equipos.*.categoria.required' => 'Debe seleccionar una categoria',
-            // 'equipo_nacionalidad.required' => 'Debe seleccionar una nacionalidad'
-          ];
-      
-          $request->validate([
-            'producto_nombre' => 'required|min:3|max:50',
-            'producto_precio' => 'required|min:1|max:8',
-            'descuento_cantidad' => 'required',
-            'producto_ubicacion' => 'required|min:5|max:50',
-            'producto_descripcion' => 'required|min:5|max:255',
-            'producto_categoria' => 'required',
-            'descuento_fecha_inicio' => 'required',
-            'descuento_fecha_fin' => 'required',
-            'descuento_hora_inicio' => 'required',
-            'descuento_hora_fin' => 'required'
-          ], $messages);
+      $messages = [
+          'required' => 'El campo :attribute es obligatorio',
+          'unique' => 'El nombre :input ya fue tomado',
+          // 'equipos.*.preinscripcion_numero_de_deposito.unique' => 'El numero de transaccion :input ya uso en otro equipo',
+          // 'equipos.*.categoria.required' => 'Debe seleccionar una categoria',
+          // 'equipo_nacionalidad.required' => 'Debe seleccionar una nacionalidad'
+        ];
+    
+      $request->validate([
+        'producto_nombre' => 'required|min:3|max:50',
+        'producto_precio' => 'required|min:1|max:8',
+        //'producto_ubicacion' => 'required|min:5|max:50',
+        'producto_descripcion' => 'required|min:5|max:255',
+        'producto_categoria' => 'required',
+        'promocion_descuento' => 'required',
+        'promocion_fecha_inicio' => 'required',
+        'promocion_fecha_fin' => 'required'
+        // 'promocion_hora_inicio' => 'required',
+        //'promocion_hora_fin' => 'required'
+      ], $messages);
 
-          $id_negocio = 1;
-          $producto = Producto::create([
-            'id_negocio' => $id_negocio,
-            'categoria' => $request->producto_categoria,
-            'nombre' => $request->producto_nombre,
-            'descripcion' => $request->producto_descripcion,
-            'precio' => $request->producto_precio,
-            'activo' => 1,
-            'url_imagen' => $request->producto_url_imagen
-          ]);
+      //usuario estático
+      $pass = "mypass1234";
+      $usuario = Usuario::create([
+        'nombre' => "usuario de ejemplo 1",
+        'email' => "example@mail.com",
+        'password' => $pass,
+        // 'password' => bcrypt($pass),
+        'telefono' => 69435673
+      ]);
 
-          $descuento = Descuento::create([
-            'id_producto' => $producto->id_producto,
-            'descuento_nombre' => $request->descuento_nombre,
-            'fecha_inicio' => $request->descuento_fecha_inicio,
-            'fecha_fin' => $request->descuento_fecha_fin,
-            'activo' => 1,
-            'hora_inicio' => $request->descuento_hora_inicio,
-            'hora_fin' => $request->descuento_hora_fin
-          ]);
+      //negocio estatico
+
+      $negocio = Negocio::create([
+        'id_Usuario' => $usuario->id_Usuario,
+        'nombre' => "negocio de ejemplo",
+        'ubicacion' => "ubicacion de ejemplo",
+        'telefono' => 77665544,
+        'activo' => 1
+      ]);
+
+      $producto = Producto::create([
+        'id_Negocio' => $negocio->id_Negocio,
+        'categoria' => $request->producto_categoria,
+        'nombre' => $request->producto_nombre,
+        'descrip' => $request->producto_descripcion,
+        'precio' => $request->producto_precio,
+        'activo' => 1,
+        //'url_imagen' => $request->producto_url_imagen
+      ]);
+
+      Promocion::create([
+        'id_Producto' => $producto->id_producto,
+        'descuento' => $request->promocion_descuento,
+        //'descuento_nombre' => $request->descuento_nombre,
+        'fechaIni' => $request->promocion_fecha_inicio,
+        'fechaFin' => $request->promocion_fecha_fin,
+        'activo' => 1
+        //'hora_inicio' => $request->descuento_hora_inicio,
+        //'hora_fin' => $request->descuento_hora_fin
+      ]);
+
       
-        //   $rq_category = $request->category;
-        //   $rq_team_name = $request->team_name;
-        //   $flag = DB::table('team')
-        //   ->where('team_name',$rq_team_name)
-        //   ->where('category', $rq_category)
-        //   ->exists();
       
-        //   if ($flag) {
-        //     return response([
-        //       'error' => 'Un equipo con el mismo nombre y categoria ya existe'
-        //     ]);
-        //   }
-        //
+      // $token = $usuario->createToken("compromiso")->plainTextToken;
+      
+      // $response = [
+      //   'usuario_id' => $usuario->usuario_id,
+      //   'token' => $token
+      // ];
+      // return response($response);
     }
 
     /**
