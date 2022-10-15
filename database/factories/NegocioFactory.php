98fe7bae641933a015ff4d864afeeb60b80c5810
Rustazaga <nevermorewars@gmail.com>
2022-10-15 17:10:48 -0400

    fabricas completas

---
+++ database/factories/NegocioFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Negocio>
+ */
+class NegocioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'id_Usuario'    => $this->faker->unique()->numberBetween(1,100),
+            'nombre'        => $this->faker->name(),
+            'descrip'       => $this->faker->sentence(),
+            'ubicacion'     => $this->faker->name(),
+            'telefono'      => $this->faker->numberBetween(4123456,4987653),
+            'horarioInicio'    => $this->faker->time(),
+            'horarioCierre'    => $this->faker->time()
+        ];
+    }
+}
--- database/seeders/NegocioSeeder.php
+++ database/seeders/NegocioSeeder.php
@@ -15,14 +15,17 @@ class NegocioSeeder extends Seeder
      */
     public function run()
     {
-        $var = new Negocio();
-        $var->id_Usuario=1;
-        $var->nombre="Pizzeria Turin";
-        $var->descrip="descripcion.....";
-        $var->ubicacion="ubicacion....";
-        $var->telefono=4453902;
-        $var->horarioInicio='18:00:00';
-        $var->horarioCierre='23:00:00';
-        $var->save();
+        //
+        //$var = new Negocio();
+        //$var->id_Usuario=1;
+        //$var->nombre="Pizzeria Turin";
+        //$var->descrip="descripcion.....";
+        //$var->ubicacion="ubicacion....";
+        //$var->telefono=4453902;
+        //$var->horarioInicio='18:00:00';
+        //$var->horarioCierre='23:00:00';
+        //$var->save();
+        Negocio::factory(100)->create();
+
     }
 }
