3c43fb9953163b64085b75a0c32a07eb779052b2
kw3a <torrezaldair777@gmail.com>
2022-11-25 22:36:30 -0400

    seeders de más productos

--- database/seeders/ProductoSeeder.php
+++ database/seeders/ProductoSeeder.php
@@ -10,43 +10,101 @@ use Illuminate\Support\Facades\Storage;
 
 class ProductoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        File::cleanDirectory(storage_path('app/images'));
-        $img_url = Storage::putFile('images', app_path('own-files/hamburguesa.jpg'));
-        Producto::create([
-            'negocio_id' => 1,
-            'nombre' => "Hamburguesa doble con queso",
-            'descrip' => "Hamburguesa que contiene dos carnes, cebolla, tomate, aderesos, queso y papas",
-            'precio' => "25",
-            'imagen' => substr($img_url, 7),
-            'categoria' => "Hamburguesa"
-        ]);
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    File::cleanDirectory(storage_path('app/images'));
 
-        $img_url = Storage::putFile('images', app_path('own-files/pizza.png'));
-        Producto::create([
-            'negocio_id' => 1,
-            'nombre' => "Pizza familiar",
-            'descrip' => "Pizza con chorizo, jamón y queso",
-            'precio' => "35",
-            'imagen' => substr($img_url, 7),
-            'categoria' => "Pizza"
-        ]);
+    //id 1
+    $img_url = Storage::putFile('images', app_path('own-files/hamburguesa.jpg'));
+    Producto::create([
+      'negocio_id' => 1,
+      'nombre' => "Hamburguesa doble con queso",
+      'descrip' => "Hamburguesa que contiene dos carnes, cebolla, tomate, aderesos, queso y papas",
+      'precio' => "25",
+      'imagen' => substr($img_url, 7),
+      'categoria' => "Hamburguesa"
+    ]);
 
-        $img_url = Storage::putFile('images', app_path('own-files/pollo.png'));
-        Producto::create([
-            'negocio_id' => 1,
-            'nombre' => "1/4 de pollo",
-            'descrip' => "Racion de papas, arroz, yuca y 2 presas de pollo",
-            'precio' => "21",
-            'imagen' => substr($img_url, 7),
-            'categoria' => "Pollo"
-        ]);
-        // Producto::factory(100)->create();
-    }
+    //id 2
+    $img_url = Storage::putFile('images', app_path('own-files/pizza.png'));
+    Producto::create([
+      'negocio_id' => 1,
+      'nombre' => "Pizza familiar",
+      'descrip' => "Pizza con chorizo, jamón y queso",
+      'precio' => "35",
+      'imagen' => substr($img_url, 7),
+      'categoria' => "Pizza"
+    ]);
+
+    //id 3
+    $img_url = Storage::putFile('images', app_path('own-files/pollo.png'));
+    Producto::create([
+      'negocio_id' => 1,
+      'nombre' => "1/4 de pollo",
+      'descrip' => "Racion de papas, arroz, yuca y 2 presas de pollo",
+      'precio' => "21",
+      'imagen' => substr($img_url, 7),
+      'categoria' => "Pollo"
+    ]);
+    
+    //id 4
+    $img_url = Storage::putFile('images', app_path('own-files/pollo.png'));
+    Producto::create([
+      'negocio_id' => 1,
+      'nombre' => "Pastel de mil hojas",
+      'descrip' => "Porción de 300gr",
+      'precio' => "8",
+      'imagen' => substr($img_url, 7),
+      'categoria' => "Postre"
+    ]);
+
+    //id 5
+    $img_url = Storage::putFile('images', app_path('own-files/pollo.png'));
+    Producto::create([
+      'negocio_id' => 1,
+      'nombre' => "Paceña Ice",
+      'descrip' => "Lata de 345 ml",
+      'precio' => "7",
+      'imagen' => substr($img_url, 7),
+      'categoria' => "Bebida"
+    ]);
+
+    //id 6
+    $img_url = Storage::putFile('images', app_path('own-files/pollo.png'));
+    Producto::create([
+      'negocio_id' => 1,
+      'nombre' => "Trucha a la parrilla",
+      'descrip' => "Trucha a la parrilla con papas y arroz",
+      'precio' => "38",
+      'imagen' => substr($img_url, 7),
+      'categoria' => "Parrilla"
+    ]);
+
+    //id 7
+    $img_url = Storage::putFile('images', app_path('own-files/pollo.png'));
+    Producto::create([
+      'negocio_id' => 1,
+      'nombre' => "Desayuno americano",
+      'descrip' => "Contiene huevos, ya sean fritos o revueltos, bacon y tortitas americanas",
+      'precio' => "12",
+      'imagen' => substr($img_url, 7),
+      'categoria' => "Desayuno"
+    ]);
+
+    //id 8
+    $img_url = Storage::putFile('images', app_path('own-files/pollo.png'));
+    Producto::create([
+      'negocio_id' => 1,
+      'nombre' => "Trucha frita",
+      'descrip' => "Trucha con ensalada criolla y papas fritas",
+      'precio' => "40",
+      'imagen' => substr($img_url, 7),
+      'categoria' => "Otros"
+    ]);
+  }
 }
--- database/seeders/PromocionSeeder.php
+++ database/seeders/PromocionSeeder.php
@@ -8,39 +8,92 @@ use App\Models\Promocion;
 
 class PromocionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // Promocion::factory(1)->create(); 
-        Promocion::create([
-            'producto_id' => 1,
-            'descuento' => 15,
-            'fecha_ini' => '2022-11-20',
-            'fecha_fin' => '2022-12-31',
-            'hora_ini' => '16:00',
-            'hora_fin' => '22:00'
-        ]);
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+      
+    //id 1
+    Promocion::create([
+      'producto_id' => 1,
+      'descuento' => 15,
+      'fecha_ini' => '2022-11-20',
+      'fecha_fin' => '2022-12-31',
+      'hora_ini' => '16:00',
+      'hora_fin' => '22:00'
+    ]);
 
-        Promocion::create([
-            'producto_id' => 2,
-            'descuento' => 25,
-            'fecha_ini' => '2022-11-18',
-            'fecha_fin' => '2022-12-31',
-            'hora_ini' => '18:00',
-            'hora_fin' => '23:00'
-        ]);
+    //id 2
+    Promocion::create([
+      'producto_id' => 2,
+      'descuento' => 25,
+      'fecha_ini' => '2022-11-18',
+      'fecha_fin' => '2022-12-31',
+      'hora_ini' => '18:00',
+      'hora_fin' => '23:00'
+    ]);
 
-        Promocion::create([
-            'producto_id' => 3,
-            'descuento' => 18,
-            'fecha_ini' => '2022-10-28',
-            'fecha_fin' => '2023-02-11',
-            'hora_ini' => '14:00',
-            'hora_fin' => '20:00'
-        ]);
-    }
+    //id 3
+    Promocion::create([
+      'producto_id' => 3,
+      'descuento' => 18,
+      'fecha_ini' => '2022-10-28',
+      'fecha_fin' => '2023-02-11',
+      'hora_ini' => '14:00',
+      'hora_fin' => '20:00'
+    ]);
+
+    //id 4
+    Promocion::create([
+      'producto_id' => 4,
+      'descuento' => 12,
+      'fecha_ini' => '2022-10-28',
+      'fecha_fin' => '2023-01-11',
+      'hora_ini' => '09:00',
+      'hora_fin' => '18:00'
+    ]);
+
+    //id 5
+    Promocion::create([
+      'producto_id' => 5,
+      'descuento' => 6,
+      'fecha_ini' => '2022-10-28',
+      'fecha_fin' => '2023-01-01',
+      'hora_ini' => '15:00',
+      'hora_fin' => '20:30'
+    ]);
+
+    //id 6
+    Promocion::create([
+      'producto_id' => 6,
+      'descuento' => 30,
+      'fecha_ini' => '2022-10-28',
+      'fecha_fin' => '2023-03-30',
+      'hora_ini' => '18:00',
+      'hora_fin' => '23:00'
+    ]);
+
+    //id 7
+    Promocion::create([
+      'producto_id' => 7,
+      'descuento' => 10,
+      'fecha_ini' => '2022-10-28',
+      'fecha_fin' => '2023-03-01',
+      'hora_ini' => '12:00',
+      'hora_fin' => '22:00'
+    ]);
+
+    //id 8
+    Promocion::create([
+      'producto_id' => 8,
+      'descuento' => 30,
+      'fecha_ini' => '2022-10-28',
+      'fecha_fin' => '2022-12-31',
+      'hora_ini' => '12:00',
+      'hora_fin' => '20:00'
+    ]);
+  }
 }
