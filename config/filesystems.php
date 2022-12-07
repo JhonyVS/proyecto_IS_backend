e5e72dd9ad7235d0b4d4a15a1c6f2c67f2e4a4d7
kw3a <torrezaldair777@gmail.com>
2022-12-07 11:30:39 -0400

    archivo de config de rutas original

--- config/filesystems.php
+++ config/filesystems.php
@@ -77,7 +77,7 @@ return [
     */
 
     'links' => [
-        base_path('/public_html/storage') => storage_path('app/public'),
+        public_path('storage') => storage_path('app/public'),
     ],
 
 ];
